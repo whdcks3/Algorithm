88660615ff87e71e58afa6321e6e1776c90251d7
JongChan <sj012944@gmail.com>
2025-09-16 15:34:34 +0900

    factorization

---
+++ Baekjoon/number/factorization.java
@@ -0,0 +1,19 @@
+package Baekjoon.number;
+
+import java.util.Scanner;
+
+public class factorization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n = n / i;
+            }
+        }
+    }
+}
+// https://www.acmicpc.net/problem/11653
